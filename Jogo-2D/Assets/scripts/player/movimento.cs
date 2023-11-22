414a66e23c1ba679527e40fb92dba6d8bbd7d49c
robertojn <127152647+robertojn@users.noreply.github.com>
2023-11-21 21:07:40 -0300

    personagem

---
+++ Jogo-2D/Assets/scripts/player/movimento.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+
+    
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(horizontal, 0, 0);
+
+    }
+}
